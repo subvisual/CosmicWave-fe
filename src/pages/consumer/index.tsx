6afa75e281560d710ec2caf83e6d935d2ea61d4b
Pedro Oliveira <poliveira@subvisual.co>
2023-06-09 14:46:31 +0100

    pages init

---
+++ src/pages/consumer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const index = (props: Props) => {
+  return <div>Hello, consumer</div>;
+};
+
+export default index;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
     <>
       <main className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-row justify-center">
-          <h1>Hello</h1>
+        <div className="flex gap-1 flex-row justify-center">
+          <Link href="/consumer">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              I'm consumer 🔊
+            </button>
+          </Link>
+          <Link href="/streamer">
+            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+              I'm streamer 🎙️
+            </button>
+          </Link>
         </div>
       </main>
     </>
