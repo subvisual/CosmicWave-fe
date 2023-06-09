474f918a8203a8ff6df4a63bf0f3395fe5f38422
Pedro Oliveira <poliveira@subvisual.co>
2023-06-09 11:11:04 +0100

    🌅

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>T</title>
+        <meta name="description" content="TODO" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <>
+      <main className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-row justify-center">
+          <h1>Hello</h1>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
