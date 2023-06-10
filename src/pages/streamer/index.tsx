1cdc84e396c16cab78659eb2672874a62992af57
Pedro Oliveira <jopedroliveira@users.noreply.github.com>
2023-06-10 09:12:25 +0100

    feat: upload file to ipfs (#2)

--- src/pages/streamer/index.tsx
+++ src/pages/streamer/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
+import UploadToIPFS from "@/components/modules/UploadToIPFS";
 
 const index = () => {
   const { address } = useAccount();
-  console.log(address);
-  return <ConnectButton />;
+  return (
+    <main className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+      <div className="flex gap-1 flex-col justify-center">
+        <ConnectButton />
+        {!!address && <UploadToIPFS />}
+      </div>
+    </main>
+  );
 };
 
 export default index;
