cade3925c8a34f132bc62f0f8d9453ede5d9e3cd
Pedro Oliveira <poliveira@subvisual.co>
2023-06-12 23:42:24 +0100

    feat: parse audio file metadata

--- src/hooks/useDatabase.tsx
+++ src/hooks/useDatabase.tsx
@@ -4,7 +4,6 @@ import { useAccount } from "wagmi";
 
 const defaultNamespace =
   "pk/0x0de76dd111433a53057babe9cf1f1d899be2ec9ea9572a8d8304494adae3521f4913b95f0c4f9f6517c45eebfdee530e8c12f859b1b915083529efc19993e280/soundverse-boilerplate";
-
 const collectionName = "MusicLibrary";
 const db = new Polybase({ defaultNamespace });
 
@@ -12,10 +11,10 @@ const useDatabase = () => {
   const { address } = useAccount();
   const [records, setRecords] = useState<any>([]);
 
-  const saveRecord = async (cid: string) => {
+  const saveRecord = async (cid: string, metadata: string = "") => {
     const result = await db
       .collection(collectionName)
-      .create([cid, address ?? "0x0"]);
+      .create([cid, address ?? "0x0", metadata]);
     return result;
   };
 
@@ -23,15 +22,6 @@ const useDatabase = () => {
     const result = await db.collection(collectionName).record(cid).get();
     return result;
   };
-
-  // const getAllRecords = async () => {
-  //   if (!address) return [];
-  //   const result = await db
-  //     .collection(collectionName)
-  //     .where("owner", "==", address)
-  //     .get();
-  //   return result;
-  // };
 
   useEffect(() => {
     if (address) {
