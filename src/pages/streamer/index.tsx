008cd7528b75766ee2c0e03a842bf20dec5dbb76
Pedro Oliveira <jopedroliveira@users.noreply.github.com>
2023-06-16 15:03:52 +0100

    feat: add ens name and profile boilerplate (#7)

--- src/pages/streamer/index.tsx
+++ src/pages/streamer/index.tsx
@@ -1,16 +1,45 @@
 import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount } from "wagmi";
 import UploadToIPFS from "@/components/modules/UploadToIPFS";
+import { useEnsAvatar, useAccount, useEnsName } from "wagmi";
 
 const index = () => {
   const { address } = useAccount();
+
+  const {
+    data: ensName,
+    isError,
+    isLoading,
+  } = useEnsName({
+    address,
+  });
+
+  const {
+    data: ensAvatar,
+    isError: isAvatarError,
+    isLoading: isAvatarLoading,
+  } = useEnsAvatar({
+    name: ensName,
+  });
+
+  console.log(ensAvatar);
+
   return (
     <main className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
       <div className="flex gap-1 flex-col justify-center">
         <ConnectButton />
         {!!address && (
           <>
+            <div className="flex flex-col text-center justify-center">
+              {isAvatarLoading && <div>Fetching avatar</div>}
+              {isAvatarError && <div>Error fetching avatar</div>}
+              {ensAvatar && (
+                <img height={50} width={50} src={ensAvatar} alt="avatar" />
+              )}
+              {isLoading && <div>Fetching name</div>}
+              {isError && <div>Error fetching name</div>}
+              <h1 className="text-2xl font-bold">Welcome {ensName}</h1>
+            </div>
             <UploadToIPFS />
           </>
         )}
