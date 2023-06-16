11e47dc17e847011dc71d9649626c7461144ee6c
Pedro Oliveira <poliveira@subvisual.co>
2023-06-16 12:07:04 +0100

    fixup: Replace player component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <main className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
       <div className="flex flex-row gap-4 justify-center items-center">
-        {/* <Player /> */}
+        <Player />
         <Link href="/streamer">
           <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
             I am streamer 🎙️
