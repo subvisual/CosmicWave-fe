72a9ec724d00a4fe62306cffacb5715768d47287
Pedro Oliveira <poliveira@subvisual.co>
2023-06-18 10:18:32 +0100

    wip

--- src/components/elements/FileManager.tsx
+++ src/components/elements/FileManager.tsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 const FileManager = () => {
-  const songs = [
-    { name: "song 1", cid: "123123" },
-    { name: "song 2", cid: "312312" },
-    { name: "song 3", cid: "434234" },
-    { name: "song 4", cid: "123123" },
-    { name: "song 5", cid: "312382" },
-    { name: "song 6", cid: "434294" },
-    { name: "song 7", cid: "1231623" },
-    { name: "song 8", cid: "3123412" },
-    { name: "song 9", cid: "43427634" },
+  const songsList = [
+    { checked: false, name: "song 1", cid: "123123" },
+    { checked: false, name: "song 2", cid: "312312" },
+    { checked: false, name: "song 3", cid: "434234" },
+    { checked: false, name: "song 4", cid: "1231223" },
+    { checked: false, name: "song 5", cid: "312382" },
+    { checked: false, name: "song 6", cid: "434294" },
+    { checked: false, name: "song 7", cid: "1231623" },
+    { checked: false, name: "song 8", cid: "3123412" },
+    { checked: false, name: "song 9", cid: "43427634" },
   ];
 
-  const handleAddToPlaylistButtonClick = () => {};
-  const handleSelectAllCheckboxChange = () => {};
-  const handleSubmitFiles = () => {};
+  const [songs, setSongs] = useState(songsList);
 
-  const checkedSongs = [];
+  const handleAddToPlaylistButtonClick = (e: any) => {
+    const songId = e.target.id;
+    const checkedInput = e.target.checked;
+    const selectedSong = songs.filter((s) => s.cid === songId);
+    const newSongs = selectedSong
+      ? [...songs, { ...selectedSong[0], checked: checkedInput }]
+      : songs;
+    setSongs(newSongs);
+  };
+
+  const handleSelectAllCheckboxChange = (e: any) => {
+    const checkedInput = e.target.checked;
+    const newSongs = songs.map((s) => ({ ...s, checked: checkedInput }));
+    setSongs(newSongs);
+  };
+  const handleSubmitFiles = () => {};
 
   return (
     <div className="m-1 h-96">
@@ -32,12 +45,12 @@ const FileManager = () => {
       <div className="grid grid-cols-3 gap-4 w-full">
         <div className="col-span-1"></div>
         <div className="col-span-2">
-          <div className="flex m-1 px-6 items-center">
+          <div className="flex m-1 px-6 items-center ">
             <input
               id=""
               type="checkbox"
               value=""
-              className="w-4 h-4focus:ring-2"
+              className="w-4 h-4focus:ring-2 accent-transparent"
               onChange={handleSelectAllCheckboxChange}
             />
             <label
@@ -86,9 +99,10 @@ const FileManager = () => {
                   className="w-full flex items-center py-3 hover:bg-slate-700 hover:bg-opacity-50 cursor-pointer pl-7"
                 >
                   <input
-                    id=""
+                    id={song.cid}
                     type="checkbox"
-                    value=""
+                    checked={song.checked}
+                    onChange={handleAddToPlaylistButtonClick}
                     className="w-4 h-4 focus:ring-2 accent-transparent"
                   />
                   <label
