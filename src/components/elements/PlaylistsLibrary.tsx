0fd46cc9cd842b47c1af5a184f3ac48183ea7dbe
joaop21 <jpsilva9898@gmail.com>
2023-06-20 12:26:08 +0100

    listen for new playlists

--- src/components/elements/PlaylistsLibrary.tsx
+++ src/components/elements/PlaylistsLibrary.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Button from "@/components/elements/Button";
+import { Polybase } from "@polybase/client";
+
+interface ForeignKey {
+  collection_id: string;
+  id: string;
+}
+
+interface Playlist {
+  id: string;
+  songs: ForeignKey[];
+  owner: ForeignKey;
+}
+
+const db = new Polybase({
+  defaultNamespace:
+    "pk/0x98550a271a85832718f29cf70384e551b852ada0beec830f9c682b7de22d945ad828dbc50de17194936565d27ef6da583c8e8856d7f27bbd97b34419401e5b47/SoundverseTest3",
+});
 
 const PlaylistsLibrary = () => {
   // const playlists = ["playlist1", "playlist2", "playlist3"];
-  const playlists: any[] = [];
+  // const playlists: any[] = [];
   // todo - get playlists
   // todo - stream
+
+  const [playlists, setPlaylists] = React.useState<Playlist[]>([]);
+
+  useEffect(() => {
+    db.collection<Playlist>("Playlist").onSnapshot((changes) => {
+      setPlaylists(changes.data.map((change) => change.data));
+    });
+  }, [playlists]);
 
   const handleStreamButtonClick = () => {};
   return (
@@ -17,10 +42,10 @@ const PlaylistsLibrary = () => {
           <ul>
             {playlists.map((playlist) => (
               <li
-                key={playlist}
+                key={playlist.id}
                 className="flex flex-row place-content-between items-center group"
               >
-                <h1 className="text-white font-md font-light">{playlist}</h1>
+                <h1 className="text-white font-md font-light">{playlist.id}</h1>
                 <Button
                   type="button"
                   handleClick={handleStreamButtonClick}
